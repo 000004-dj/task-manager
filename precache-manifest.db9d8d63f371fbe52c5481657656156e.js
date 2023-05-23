d3ad1a540467087e811f3530ea0d9ea3709a88f4
Samuil Prytchyn <79350951+000004-dj@users.noreply.github.com>
2023-05-24 00:00:01 +0200

    Updates

---
+++ precache-manifest.db9d8d63f371fbe52c5481657656156e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597a74253c2421a0795679702665b7b3",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "bff6843b7af7c26ed32d",
+    "url": "/task-manager/static/css/main.16fce336.chunk.css"
+  },
+  {
+    "revision": "d895d918dfccd916c5b3",
+    "url": "/task-manager/static/js/2.7454b609.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/task-manager/static/js/2.7454b609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff6843b7af7c26ed32d",
+    "url": "/task-manager/static/js/main.dea32987.chunk.js"
+  },
+  {
+    "revision": "bc500f03bcb32101dfe5",
+    "url": "/task-manager/static/js/runtime-main.f41001b5.js"
+  },
+  {
+    "revision": "7885c42bfec02e268609cc82927e8317",
+    "url": "/task-manager/static/media/background.7885c42b.jpg"
+  }
+]);
